62e82e8f3b5cfd43397f5149add35e3a544449a0
poppq <poppq03@naver.com>
2026-07-08 15:09:30 +0900

    feat: ErrorCode, ApiResponse 작성 #17

---
+++ src/main/java/com/found404/delivery/global/response/ApiResponse.java
@@ -0,0 +1,42 @@
+package com.found404.delivery.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final T data;
+    private final ErrorDetail error;
+
+    private ApiResponse(boolean success, T data, ErrorDetail error) {
+        this.success = success;
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, null, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String code, String message) {
+        return new ApiResponse<>(false, null, new ErrorDetail(code, message));
+    }
+
+    @Getter
+    public static class ErrorDetail {
+        private final String code;
+        private final String message;
+
+        public ErrorDetail(String code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+}
