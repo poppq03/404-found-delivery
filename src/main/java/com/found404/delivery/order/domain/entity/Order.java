9e38dbb22f88fb68d999919600d5ce97ee8653c4
woohyun23 <parkwh0823@gmail.com>
2026-07-09 12:33:24 +0900

    feat: entity 생성
    Address, Order, OrderItem

---
+++ src/main/java/com/found404/delivery/order/domain/entity/Order.java
@@ -0,0 +1,69 @@
+package com.found404.delivery.order.domain.entity;
+
+import com.found404.delivery.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "p_order",
+        indexes = {
+                @Index(name = "idx_p_order_user_id", columnList = "user_id"),
+                @Index(name = "idx_p_order_store_id", columnList = "store_id"),
+                @Index(name = "idx_p_order_status", columnList = "status")
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+    @Id
+    @UuidGenerator
+    @Column(name = "order_id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "store_id", nullable = false)
+    private UUID storeId;
+
+    @Column(name = "address_id", nullable = false)
+    private UUID addressId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status = OrderStatus.REQUESTED;
+
+    @Column(name = "total_menu_price", nullable = false)
+    private int totalMenuPrice;
+
+    @Column(name = "delivery_fee", nullable = false)
+    private int deliveryFee = 0;
+
+    @Column(name = "discount_price", nullable = false)
+    private int discountPrice = 0;
+
+    @Column(name = "total_price", nullable = false)
+    private int totalPrice;
+
+    @Column(name = "delivery_address", nullable = false, length = 255)
+    private String deliveryAddress;
+
+    @Column(name = "delivery_detail_address", length = 255)
+    private String deliveryDetailAddress;
+
+    @Column(name = "delivery_request", length = 255)
+    private String deliveryRequest;
+
+    @Column(name = "canceled_at")
+    private LocalDateTime canceledAt;
+
+    @Column(name = "status_reason", length = 255)
+    private String statusReason;
+}
---
+++ src/main/java/com/found404/delivery/order/domain/entity/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.found404.delivery.order.domain.entity;
+
+public enum OrderStatus {
+    REQUESTED,
+    ACCEPTED,
+    REJECTED,
+    COOKING,
+    DELIVERING,
+    COMPLETED,
+    CANCELED
+}
