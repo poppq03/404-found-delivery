934476cd36165650da96cb1f463347471a3cc50d
SiruDuck <potato32122@gmail.com>
2026-07-15 17:31:32 +0900

    feature: StoreMaster 상태변경/삭제 API 구현

--- src/main/java/com/found404/delivery/domain/store/controller/StoreMasterController.java
+++ src/main/java/com/found404/delivery/domain/store/controller/StoreMasterController.java
@@ -1,12 +1,19 @@
 package com.found404.delivery.domain.store.controller;
 
-import com.found404.delivery.domain.store.dto.response.StoreSimpleResponseDto;
+import com.found404.delivery.domain.store.dto.request.StoreStatusRequestDto;
+import com.found404.delivery.domain.store.dto.response.StorePendingResponseDto;
 import com.found404.delivery.domain.store.dto.response.StoreStatusResponseDto;
 import com.found404.delivery.domain.store.service.StoreService;
+import com.found404.delivery.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,29 +23,48 @@ public class StoreMasterController {
 
     private final StoreService storeService;
 
+    // 가게 승인 대기 목록?
+    @GetMapping ("/stores/approval")
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER')")
+    public Slice<StorePendingResponseDto> getPendingStores (
+            @PageableDefault(size = 20 , sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable
+    ) {
+
+        return storeService.getPendingStores(pageable);
+    }
+
 
     // 가게 승인
     @PatchMapping("/stores/{storeId}/approve")
-    public StoreStatusResponseDto storeApproval (@PathVariable UUID storeId) {
-        return null;
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER')")
+    public StoreStatusResponseDto storeApproval (
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable UUID storeId) {
+        return storeService.storeApproval(userDetails.getUserId(), storeId);
     }
 
-    // 가게 승인 대기 목록?
-    @GetMapping ("/stores/approval")
-    public List<StoreSimpleResponseDto> storeApprovalList () {
-        return null;
-    }
 
     // 가게 상태변경
-    @PatchMapping("/stores/{storeId}/suspend")
-    public StoreStatusResponseDto updateStoreSuspend(@PathVariable UUID storeId){
-        return null;
+    @PatchMapping("/stores/{storeId}/status")
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER')")
+    public StoreStatusResponseDto updateStoreStatus(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable UUID storeId,
+            @RequestBody StoreStatusRequestDto request){
+        return storeService.updateStoreStatusByMaster(userDetails.getUserId(),storeId, request);
     }
 
     // 가게 삭제
     @DeleteMapping("/stores/{storeId}")
-    public StoreStatusResponseDto deleteStore(@PathVariable UUID storeId){
-        return null;
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER')")
+    public StoreStatusResponseDto deleteStore(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable UUID storeId
+    ){
+        return storeService.deleteStoreByMaster(
+                userDetails.getUserId(),
+                storeId
+        );
     }
 
 
